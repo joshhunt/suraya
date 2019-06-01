f1f2eed3910795df234663ede5560dca715f6d17
Josh Hunt <josh@trtr.co>
2019-06-01 10:10:49 +0100

    sort order

--- src/store/PERK_SORT_ORDER.js
+++ src/store/PERK_SORT_ORDER.js
@@ -2,5 +2,41 @@ export default [
   0, // spacer
   143442373, // Primary Ammo Finder
   2620835322, // Special Ammo Finder
-  2867719094 // Heavy Ammo Finder
+  2867719094, // Heavy Ammo Finder
+
+  2433223313, // Sidearm Reserves
+  833036833, // Hand Cannon Reserves
+  1879053658, // Bow Reserves
+  3910640537, // Scout Rifle Reserves
+  1659799750, // Pulse Rifle Reserves
+  2928640162, // Auto Rifle Reserves
+  3975312523, // Submachine Gun Reserves
+
+  1068909436, // Shotgun Reserves
+  4034210761, // Fusion Rifle Reserves
+  1180144002, // Sniper Rifle Reserves
+
+  2318225451, // Sword Reserves
+  901125364, // Machine Gun Reserves
+  4007230256, // Linear Fusion Rifle Reserves
+  330650351, // Grenade Launcher Reserves
+  1812356167, // Rocket Launcher Reserves
+
+  3670792765, // Sidearm Scavenger
+  4047893141, // Hand Cannon Scavenger
+  735778136, // Arrow Scavenger
+  1507173193, // Scout Rifle Scavenger
+  3081361536, // Pulse Rifle Scavenger
+  3784207446, // Auto Rifle Scavenger
+  699706967, // Submachine Gun Scavenger
+
+  1066067378, // Shotgun Scavenger
+  127731051, // Fusion Rifle Scavenger
+  2600330634, // Sniper Rifle Scavenger
+
+  2034622599, // Sword Scavenger
+  790090923, // Machine Gun Scavenger
+  3054567270, // Linear Fusion Rifle Scavenger
+  866518141, // Grenade Launcher Scavenger
+  4245438319 // Rocket Launcher Scavenger
 ];
